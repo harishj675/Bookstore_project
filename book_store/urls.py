88721b70507f86bac423f7bc2f2226e22df53fb4
harishj675 <harish.jadhav@successive.tech>
2024-09-02 10:34:35 +0530

    updated

--- book_store/urls.py
+++ book_store/urls.py
@@ -9,15 +9,14 @@ schema_view = SpectacularAPIView.as_view()
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', include('bookstore.urls')),  # Main bookstore URLs
-                  path('accounts/', include('django.contrib.auth.urls')),  # Authentication URLs
-                  path('users/', include('users.urls')),  # User-specific URLs
-                  path('cart/', include('cart.urls')),  # Cart-related URLs
-                  path('api/bookstore/', include('bookstore.api.urls')),  # API endpoints for bookstore
-                  path('api/users/', include('users.api.urls')),  # API endpoints for users
-                  path('api/cart/', include('cart.api.urls')),  # API endpoints for cart
+                  path('', include('bookstore.urls')),
+                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('users/', include('users.urls')),
+                  path('cart/', include('cart.urls')),
+                  path('api/bookstore/', include('bookstore.api.urls')),
+                  path('api/users/', include('users.api.urls')),
+                  path('api/cart/', include('cart.api.urls')),
 
-                  # API schema and documentation URLs
                   path('api/schema/', schema_view, name='schema'),
                   path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
                   path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
